7a3d137820d8230210193ffb76074763e574400b
Martin Terence <155010370+MartinTerence@users.noreply.github.com>
2023-12-27 19:53:49 +0200

    Update flow calibration.dart

--- flow calibration.dart
+++ flow calibration.dart
@@ -8,6 +8,13 @@ class MockWallet extends Mock implements PylonsWallet {
   }
 }
 
+const String MOCK_USERNAME = 'Jawad';
+const String MOCK_ERR_CODE = 'Something went wrong';
+const String MOCK_EXECUTION_ID = 'executionid';
+const String MOCK_RECIPE_VERSION = 'v0.1.3';
+Int64 MOCK_NODE_VERSION = Int64(1);
+
+
 import 'package:pylons_sdk/src/generated/pylons/item.pb.dart';
 import 'package:pylons_sdk/src/generated/pylons/recipe.pb.dart';
 import 'package:pylons_sdk/src/generated/pylons/trade.pb.dart';
